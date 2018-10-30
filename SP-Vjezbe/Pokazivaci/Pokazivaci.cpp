e7cb0bf90ed6e0a1090a45a093be063421e6185a
Danijel <dzelenik@bitbucket.org>
2018-10-30 09:11:27 +0100

    Vježbe 30.10.2018.

--- SP-Vjezbe/Pokazivaci/Pokazivaci.cpp
+++ SP-Vjezbe/Pokazivaci/Pokazivaci.cpp
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
+void funkcija(int *p)
+{
+	*p = *p * 2;
+}
+
 void main() {
-	printf("Hello!");
+
+	int a = 5;
+	int *b = &a;
+	printf("Adresa a: %d %d", &a, a);
+	printf("\n*b = %d", *b);
+
+	funkcija(&a);
+	
+	printf("\n%d", a);
 
 	getchar();
 }
