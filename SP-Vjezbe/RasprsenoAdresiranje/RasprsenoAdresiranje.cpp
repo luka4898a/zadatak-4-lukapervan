645a6bec292d66cbdb3fbfb69fca2917ed489466
lukapervan <luka.pervan@student.fsr.ba>
2018-11-30 21:49:53 +0100

    RasprsenoAdresiranje

--- SP-Vjezbe/RasprsenoAdresiranje/RasprsenoAdresiranje.cpp
+++ SP-Vjezbe/RasprsenoAdresiranje/RasprsenoAdresiranje.cpp
@@ -60,16 +60,55 @@ void format(FILE *f)
 
 //Zadaća
 //1. Zadatak
+/*Napisati funkciju za upis šifre (cijeli broj) i naziva (20+1) u memorijski rezidentnu tablicu raspršenih adresa s 
+500 pretinaca. Pretinac sadrži jedan zapis. Ako je pretinac popunjen, prelazi se ciklički na susjedni. Ulazni argumenti su 
+već izračunata adresa pretinca, šifra i naziv. Funkcija vraća vrijednost 1 ako je upis obavljen, 0 ako podatak već postoji, 
+a -1 ako je tablica popunjena pa se podatak nije mogao upisati.*/
 zapis tablica[MAX];
 int upis(int adresa, zapis pom)
 {
-	return -1;
+	for (int i = 0; i < MAX; i++)
+	{
+		if (tablica[i].sifra == pom.sifra)
+			return 0;
+
+		if else (tablica[i].sifra == 0)
+		{
+			tablica[adresa].sifra = pom.sifra;
+			return 1;
+		}
+
+		if else
+			continue;
+
+		else
+			return -1;
+
+	}
+
 }
 
 //2. Zadatak
+/*Napisati funkciju za pronalaženje šifre (cijeli broj) i naziva poduzeća (30+1) iz memorijski rezidentne tablice raspršenih 
+adresa s 200 pretinaca. Pretinac sadrži jedan zapis. Ako je pretinac popunjen, a ne sadrži traženu vrijednost ključa, 
+prelazi se ciklički na susjedni. Ulazni argumenti su već izračunata adresa pretinca i šifra. Izlazni argument je naziv 
+poduzeća. Funkcija vraća vrijednost 1 ako je zapis pronađen, a 0 ako nije.*/
 int pronadji(int adresa, int sifra, char *naziv)
 {
-	return -1;
+
+	for (int i = 0; i < MAX; i++)
+	{
+
+		if((adresa=i)&&(sifra[i]!=0)==0)
+			return 1;
+		else
+		{
+			printf("Podatak ne postoji!");
+			return 0;
+		}
+
+	}
+
 }
 
 int main()
@@ -81,7 +120,6 @@ int main()
 	FILE *f = fopen(fout, "r+");
 	if (f == NULL)
 		return -1;
-
 	//format(f);
 	int br = brojZapisa(f);
 	
